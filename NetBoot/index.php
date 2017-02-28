5f0cab1b2cc3b749ce112460c4d3278939f49900
DeniZ Dobynda <Falling-in-l0ve>
2017-02-28 09:38:22 +0200

    clear files

--- NetBoot/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!isset($_COOKIE['user'])) {
-    header("Location: login.php");
-} else {
-    $_SESSION['user'] = $_COOKIE['user'];
-    header("Location: profile.php");
-}
-?>
--- NetBoot/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include_once("functionality.php");
-clearSessionAndCookie();
-header("Location: index.php");
-?>
